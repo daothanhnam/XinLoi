5d3113ad7b36b75a14c71dd78aa675f81a3d2bbd
Daothanhnam_T2108A <88824678+daothanhnam@users.noreply.github.com>
2022-03-28 13:35:06 +0700

    Add files via upload

---
+++ ToXinLoiDan/anhyeuDana.java
@@ -0,0 +1,12 @@
+package com.ToXinLoiDan;
+
+public class anhyeuDana {
+    public static void main(String[] arg){
+        String xinloi = "Anh xin lỗi";
+
+        for (int i =1; i<=1000; i++){
+            System.out.println(i + ". Anh xin loi Dan a");
+        }
+
+    }
+}
